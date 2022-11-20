e6fe90835315fa1b8b59232293b6d6cbf4d067c7
sergyth <73670477+sergyth@users.noreply.github.com>
2022-11-20 19:53:26 +0100

    suppression des tableaux codes et countries

--- index.js
+++ index.js
@@ -3,9 +3,7 @@ const bandiera = document.querySelector('.bandiera');
 const bandieraName = document.querySelector('.bandiera-name');
 
 
-let entries = new Set();
-let codes = new Set();
-let countries = new Set();
+let entries = [];
 let randomCode = '';
 
 schiacciami.addEventListener('click', () => {
@@ -14,30 +12,23 @@ schiacciami.addEventListener('click', () => {
     .then(res => res.json())
     .then((data) => {
         entries = Object.entries(data);
-        entries.map((entry) => {
-          codes.add(entry[0])
-        })
+        randomCode = Math.floor(Math.random() * entries.length);
 
-        entries.map((entry) => {
-            countries.add(entry[1]) 
-        })
-            randomCode = Math.floor(Math.random() * codes.size);
-            let addFlags = 
-            `<img
-            src="https://flagcdn.com/${[...codes][randomCode] }.svg"
-            width="200"
-            alt="">`;
-            let addFlagsName = `
-            <span>${[...countries][randomCode]}`
-              
-            bandiera.innerHTML = '';
-            bandiera.innerHTML = addFlags;  
-            bandieraName.innerHTML = '';  
-            bandieraName.innerHTML = addFlagsName;
-            bandieraName.classList.add('black');  
-       
-    })
-  
+        let addFlags = 
+        `<img
+        src="https://flagcdn.com/${entries[randomCode][0] }.svg"
+        width="200"
+        alt="">`;
+
+        let addFlagsName = `
+        <span>${entries[randomCode][1]}</span>`
+            
+        bandiera.innerHTML = '';
+        bandiera.innerHTML = addFlags;  
+        bandieraName.innerHTML = '';  
+        bandieraName.innerHTML = addFlagsName;
+        bandieraName.classList.add('black');     
+    }) 
 })
 
 bandieraName.addEventListener('click', () => {
